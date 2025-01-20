dc918cb276aa8d624b3f6e07bc8eb90d91f4a723
rafi_ahmed <rafiiitju282@gmail.com>
2025-01-20 20:57:07 +0600

    worked on responsiveness

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -32,7 +32,7 @@ const Dashboard = () => {
        <Helmet>
             <title>Dashboard | Medimart</title>
         </Helmet>
-       <button className="flex lg:hidden p-4 md:p-10 mr-10" onClick={toggleDrawer} >
+       <button className="flex lg:hidden p-4 md:p-10 mr-10 z-50" onClick={toggleDrawer} >
           <FaBars className="text-4xl" />
         </button>
       <div className="">
@@ -259,11 +259,112 @@ const Dashboard = () => {
           </div>
         )}
       </div>
-      <div className="flex lg:hidden">
+      
+      <div className="flex lg:hidden bg-btns z-50 h-full">
         
-        <Drawer open={isOpen} onClose={toggleDrawer} direction="left">
-          {role === "admin" && (
-            <div className="  flex flex-col gap-8  ">
+        <Drawer open={isOpen} onClose={toggleDrawer} direction="left" className="bg-btns z-50">
+        {role === "admin" && (
+          <div className="  flex flex-col gap-2 z-50 py-10">
+            <NavLink
+              to="/dashboard/home"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold  border-b-4 p-3 rounded-sm"
+              }
+            >
+              <span className="flex items-center justify-between">
+                Admin Home <FaHome className="text-xl" />
+              </span>
+            </NavLink>
+
+            <NavLink
+              to="/dashboard/users"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold  border-b-4 p-3 rounded-sm"
+              }
+            >
+              <span className="flex items-center justify-between">
+                Manage Users <FaUsers className="text-xl" />
+              </span>
+            </NavLink>
+
+            <NavLink
+              to="/dashboard/medicineCategory"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold  border-b-4 p-3 rounded-sm"
+              }
+            >
+              <span className="flex items-center justify-between">
+                Manage Category <FaList className="text-xl" />
+              </span>
+            </NavLink>
+
+            <NavLink
+              to="/dashboard/paymentManagement"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold  border-b-4 p-3 rounded-sm"
+              }
+            >
+              <span className="flex items-center justify-between">
+                Payment management
+                <FaCreditCard className="text-xl" />
+              </span>
+            </NavLink>
+
+            <NavLink
+              to="/dashboard/salesReport"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold  border-b-4 p-3 rounded-sm"
+              }
+            >
+              <span className="flex items-center justify-between">
+                {" "}
+                Sales Report
+                <FaChartLine className="text-xl" />
+              </span>
+            </NavLink>
+
+            <NavLink
+              to="/dashboard/manageBannerAdd"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold  border-b-4 p-3 rounded-sm"
+              }
+            >
+              <span className="flex items-center justify-between gap-4">
+                Manage banner Advertise
+                <FaAd className="text-xl" />
+              </span>
+            </NavLink>
+
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold  border-b-4 p-3 rounded-sm"
+              }
+            >
+              <span className="flex items-center justify-between">
+                Home
+                <FaHome className="text-xl" />
+              </span>
+            </NavLink>
+          </div>
+        )}{" "}
+        {role === "seller" && (
+          <div className="  flex flex-col gap-8  py-20">
+            {/* 
               <NavLink
                 to="/dashboard/home"
                 className={({ isActive }) =>
@@ -272,139 +373,72 @@ const Dashboard = () => {
                     : "font-bold"
                 }
               >
-                Admin Home
+                Seller Home
               </NavLink>
+             */}
 
-              <NavLink
-                to="/dashboard/users"
-                className={({ isActive }) =>
-                  isActive
-                    ? "bg-[#439A97] font-bold text-white lg:text-lg"
-                    : "font-bold"
-                }
-              >
-                Manage Users
-              </NavLink>
+            <NavLink
+              to="/dashboard/manageMedicine"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold text-xl border-b-4 p-3 rounded-sm"
+              }
+            >
+              <span className="flex items-center justify-between gap-4">
+              Manage Medicines
+                <FaPlus className="text-4xl" />
+                </span>
+            </NavLink>
 
-              <NavLink
-                to="/dashboard/medicineCategory"
-                className={({ isActive }) =>
-                  isActive
-                    ? "bg-[#439A97] font-bold text-white lg:text-lg"
-                    : "font-bold"
-                }
-              >
-                Manage Category
-              </NavLink>
-
-              <NavLink
-                to="/dashboard/paymentManagement"
-                className={({ isActive }) =>
-                  isActive
-                    ? "bg-[#439A97] font-bold text-white lg:text-lg"
-                    : "font-bold"
-                }
-              >
-                Payment management
-              </NavLink>
-
-              <NavLink
-                to="/dashboard/salesReport"
-                className={({ isActive }) =>
-                  isActive
-                    ? "bg-[#439A97] font-bold text-white lg:text-lg"
-                    : "font-bold"
-                }
-              >
-                Sales Report
-              </NavLink>
-
-              <NavLink
-                to="/dashboard/manageBannerAdd"
-                className={({ isActive }) =>
-                  isActive
-                    ? "bg-[#439A97] font-bold text-white lg:text-lg"
-                    : "font-bold"
-                }
-              >
-                Manage banner Advertise
-              </NavLink>
-
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive
-                    ? "bg-[#439A97] font-bold text-white lg:text-lg"
-                    : "font-bold"
-                }
-              >
-                Home
-              </NavLink>
-            </div>
-          )}{" "}
-          {role === "seller" && (
-            <div className="  flex flex-col gap-8  ">
-              {/* 
-                <NavLink
-                  to="/dashboard/home"
-                  className={({ isActive }) =>
-                    isActive
-                      ? "bg-[#439A97] p-3 font-bold text-white lg:text-lg"
-                      : "font-bold"
-                  }
-                >
-                  Seller Home
-                </NavLink>
-               */}
-
-              <NavLink
-                to="/dashboard/manageMedicine"
-                className={({ isActive }) =>
-                  isActive
-                    ? "bg-[#439A97] font-bold text-white lg:text-lg"
-                    : "font-bold"
-                }
-              >
-                Manage Medicines
-              </NavLink>
-
-              <NavLink
-                to="/dashboard/paymentHistorySeller"
-                className={({ isActive }) =>
-                  isActive
-                    ? "bg-[#439A97] font-bold text-white lg:text-lg"
-                    : "font-bold"
-                }
-              >
+            <NavLink
+              to="/dashboard/paymentHistorySeller"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold text-xl border-b-4 p-3 rounded-sm"
+              }
+            >
+                <span className="flex items-center justify-between gap-4">
                 Payment History
-              </NavLink>
+                <FaCreditCard className="text-4xl" />
+              </span>
+              
+            </NavLink>
 
-              <NavLink
-                to="/dashboard/askBannerAd"
-                className={({ isActive }) =>
-                  isActive
-                    ? "bg-[#439A97] font-bold text-white lg:text-lg"
-                    : "font-bold"
-                }
-              >
-                Ask For Advertisement
-              </NavLink>
+            <NavLink
+              to="/dashboard/askBannerAd"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold text-xl border-b-4 p-3 rounded-sm"
+              }
+            >
+               <span className="flex items-center justify-between gap-4">
+               Ask For Advertisement
+                <FaAd className="text-4xl" />
+              </span>
+              
+            </NavLink>
 
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive
-                    ? "bg-[#439A97] font-bold text-white lg:text-lg"
-                    : "font-bold"
-                }
-              >
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold text-xl border-b-4 p-3 rounded-sm"
+              }
+            >
+               <span className="flex items-center justify-between">
                 Home
-              </NavLink>
-            </div>
-          )}
-          {role === "user" && (
-            <div className="  flex flex-col gap-8  ">
-              {/* 
+                <FaHome className="text-4xl" />
+              </span>
+            </NavLink>
+          </div>
+        )}
+        {role === "user" && (
+          <div className="  flex flex-col gap-8  ">
+            {/* 
                 <NavLink
                   to="/dashboard/home"
                   className={({ isActive }) =>
@@ -417,33 +451,43 @@ const Dashboard = () => {
                 </NavLink>
                */}
 
-              <NavLink
-                to="/dashboard/myPayment"
-                className={({ isActive }) =>
-                  isActive
-                    ? "bg-[#439A97] p-3 font-bold text-white lg:text-lg"
-                    : "font-bold"
-                }
-              >
-                Payment history
-              </NavLink>
+           
+            <NavLink
+              to="/dashboard/myPayment"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold text-xl border-b-4 p-3 rounded-sm"
+              }
+            >
+              
+              <span className="flex items-center justify-between gap-7
+              ">
+              Payment history
+                <FaCreditCard className="text-4xl" />
+              </span>
+            </NavLink>
 
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive
-                    ? "bg-[#439A97] font-bold text-white lg:text-lg"
-                    : "font-bold"
-                }
-              >
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-background text-btns   w-full p-3 rounded-lg font-bold text-white lg:text-lg"
+                  : "font-bold text-xl border-b-4 p-3 rounded-sm"
+              }
+            >
+               <span className="flex items-center justify-between">
                 Home
-              </NavLink>
-            </div>
-          )}
+                <FaHome className="text-4xl" />
+              </span>
+            </NavLink>
+          </div>
+        )}
         </Drawer>
       </div>
+      </div>
     </div>
-    </div>
+   
   );
 };
 
